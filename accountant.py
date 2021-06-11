df0f23272c6da69a21a297af66cf15edde55d321
xionkh1234 <83282461+xionkh1234@users.noreply.github.com>
2021-06-11 16:53:37 +0200

    Add files via upload

--- accountant.py
+++ accountant.py
@@ -1,161 +1,115 @@
 import sys
 
-# UWAGA!!! W plikach wejściowych NIE UŻYWAĆ polskich znaków!
+balance = 0
+change = 0
+magazyn = {}
+data = []
+account = {"balance": balance}
 
-if (sys.argv[1] == "saldo"):  # WYWOŁANIE: python <nazwa_programu> saldo <plik_wejsciowy>
-
-    argWartosc = input("Wprowadz wartosc: ")
-    argKomentarz = input("Wprowadz komentarz: ")
-
-    inPlik = open(sys.argv[2], "r")
-    outPlik = open("out.txt", "w")
-
-    linijki = inPlik.readlines()
-
-    for linijka in linijki:
-        if (linijka != "stop"): outPlik.write(linijka)
-
-    outPlik.write(sys.argv[1] + "\n" + argWartosc + "\n" + argKomentarz + "\n" + "stop")
-
-    inPlik.close()
-    outPlik.close()
-
-elif (sys.argv[1] == "zakup"):  # WYWOŁANIE: python <nazwa_programu> zakup <plik_wejsciowy>
-
-    argIdentyfikator = input("Wprowadz identyfikator: ")
-    argCena = input("Wprowadz cene: ")
-    argSprzedane = input("Wprowadz liczbe kupionych sztuk: ")
-
-    inPlik = open(sys.argv[2], "r")
-    outPlik = open("out.txt", "w")
-
-    linijki = inPlik.readlines()
-
-    for linijka in linijki:
-        linijka = linijka.replace("\n", "").replace(" ", "")
-        if (linijka != "stop"): outPlik.write(linijka + "\n")
-
-    outPlik.write(sys.argv[1] + "\n" + argIdentyfikator + "\n" + argCena + "\n" + argSprzedane + "\n" + "stop")
-
-    inPlik.close()
-    outPlik.close()
-
-elif (sys.argv[1] == "sprzedaz"):  # WYWOŁANIE: python <nazwa_programu> sprzedaz <plik_wejsciowy>
-
-    argIdentyfikator = input("Wprowadz identyfikator: ")
-    argCena = input("Wprowadz cene: ")
-    argSprzedane = input("Wprowadz liczbe sprzedanych sztuk: ")
-
-    inPlik = open(sys.argv[2], "r")
-    outPlik = open("out.txt", "w")
-
-    linijki = inPlik.readlines()
-
-    for linijka in linijki:
-        linijka = linijka.replace("\n", "").replace(" ", "")
-        if (linijka != "stop"): outPlik.write(linijka + "\n")
-
-    outPlik.write(sys.argv[1] + "\n" + argIdentyfikator + "\n" + argCena + "\n" + argSprzedane + "\n" + "stop")
-
-    inPlik.close()
-    outPlik.close()
-
-elif (sys.argv[1] == "magazyn"):  # WYWOŁANIE: python <nazwa_programu> magazyn <plik_wejsciowy>
-
-    argumenty = {}
-
-    argArgument = ""
-    while (True):
-        argArgument = input("Wprowadz kolejny identyfikator, wpisz 'KONIEC' aby zakonczyc: ")
-        if (argArgument == "KONIEC"):
+while True:
+    action = input().strip()
+    if action == "saldo":
+        change = int(input())
+        comment = str(input())
+        if balance + change < 0:
+            print("error")
             break
-        argumenty[argArgument] = 0
+        balance += change
+        account["balance"] =+ balance
+        data.append(["saldo", change, comment])
 
-    inPlik = open(sys.argv[2], "r")
-    outPlik = open("out.txt", "w")
+    elif action == "zakup":
+        item_id = str(input())
+        price = int(input())
+        quantity = int(input())
+        if (price * quantity) < account["balance"]:
+            balance =- price * quantity
+            account["balance"] += balance
+            if item_id in magazyn:
+                magazyn[item_id] += quantity
+            else:
+                magazyn[item_id] = quantity
+            data.append(["zakup", item_id, price, quantity])
+        if price < 0 or quantity <= 0:
+            print("Nieprawidłowe wartości.")
+            break
 
-    linijki = inPlik.readlines()
+    elif action == "sprzedaz":
+        item_id = str(input())
+        price = int(input())
+        quantity = int(input())
+        if item_id in magazyn:
+            magazyn[item_id] -= quantity
+            balance = + price * quantity
+            account["balance"] += balance
+            data.append(["sprzedaz", item_id, price, quantity])
+        elif item_id not in magazyn:
+            print("Pusty magazyn")
+            break
 
-    for i, linijka in enumerate(linijki):
-        linijka = linijka.replace("\n", "").replace(" ", "")
-        if (linijka in argumenty):
-            typTransakcji = linijki[i - 1].replace("\n", "")
-            if (typTransakcji == "zakup"):
-                argumenty[linijka] += int(linijki[i + 2])
-            elif (typTransakcji == "sprzedaz"):
-                argumenty[linijka] -= int(linijki[i + 2])
+    elif action == "stop":
+        break
 
-    for argument in argumenty:
-        print(argument + ": " + str(argumenty[argument]))
-        outPlik.write(argument + ": " + str(argumenty[argument]) + "\n")
 
-    inPlik.close()
-    outPlik.close()
+if sys.argv[1] == "sprzedaz":
+    item_id = sys.argv[2]
+    price = int(sys.argv[3])
+    quantity = int(sys.argv[4])
+    if item_id in magazyn:
+        balance = + price * quantity
+        account["balance"] += balance
+        data.append([sys.argv[1], item_id, price, quantity])
+        magazyn[item_id] -= ilosc
+    else:
+        print("Brak towaru")
+    for list in data:
+        for item in list:
+            print(item)
+    print("stop")
 
-elif (sys.argv[1] == "konto"):  # WYWOŁANIE: python <nazwa_programu> konto <plik_wejsciowy>
+if sys.argv[1] == "zakup":
+    item_id = sys.argv[2]
+    price = int(sys.argv[3])
+    quantity = int(sys.argv[4])
+    if (price * quantity) < account["balance"]:
+        balance = - price * quantity
+        account["balance"] += balance
+        if item_id in magazyn:
+            magazyn[item_id] += quantity
+        else:
+            magazyn[item_id] = quantity
+        data.append(["zakup", item_id, price, quantity])
+    if price < 0 or quantity <= 0:
+        print("Nieprawidłowe wartości.")
+    for list in data:
+        for item in list:
+            print(item)
+    print("stop")
 
-    inPlik = open(sys.argv[2], "r")
-    outPlik = open("out.txt", "w")
+if sys.argv[1] == "magazyn":
+    item_id = str(sys.argv[4])
+    quantity = 0
+    if item_id not in magazyn:
+        magazyn[item_id] = ilosc
+    if item_id in magazyn:
+        for produkt, quantity in magazyn.items():
+            print("{}: {}".format(produkt, quantity))
 
-    suma = 0
-    sumowanieSaldo = False
-    sumowanieZakup = False
-    sumowanieSprzedaz = False
 
-    linijki = inPlik.readlines()
+if sys.argv[1] == "przeglad":
+    start = 0
+    end = 0
+    if len(sys.argv) > 2:
+        start = sys.argv[2]
+        koniec = sys.argv[3]
+    for list in data[int(start):int(end)]:
+        for item in list:
+            print(item)
+    print("stop")
 
-    for i, linijka in enumerate(linijki):
-        linijka = linijka.replace("\n", "").replace(" ", "")
 
-        if (sumowanieSaldo == True):
-            suma += int(linijka)
-            sumowanieSaldo = False
-        elif (sumowanieZakup == True):
-            if (int(linijki[i + 1]) < 0 or int(linijki[i + 2]) < 0): print("Blad!")
-            suma -= int(linijki[i + 1]) * int(linijki[i + 2])
-            sumowanieZakup = False
-        elif (sumowanieSprzedaz == True):
-            if (int(linijki[i + 1]) < 0 or int(linijki[i + 2]) < 0): print("Blad!")
-            suma += int(linijki[i + 1]) * int(linijki[i + 2])
-            sumowanieSprzedaz = False
+if sys.argv[1] == "saldo":
+    print(account["balance"])
 
-        if (linijka == "saldo"):
-            sumowanieSaldo = True
-        elif (linijka == "zakup"):
-            sumowanieZakup = True
-        elif (linijka == "sprzedaz"):
-            sumowanieSprzedaz = True
-
-    if (suma < 0): print("Blad!")
-
-    print(suma)
-    outPlik.write(str(suma))
-    inPlik.close()
-    outPlik.close()
-
-elif (sys.argv[1] == "przeglad"):  # WYWOŁANIE: python <nazwa_programu> przeglad <index_od> <index_do> <plik_wejsciowy>
-    inPlik = open(sys.argv[4], "r")
-    outPlik = open("out.txt", "w")
-
-    linijki = inPlik.readlines()
-
-    licznik = 0
-    for i, linijka in enumerate(linijki):
-        if (i < int(sys.argv[2])): continue
-        linijka = linijka.replace("\n", "")
-        if (linijka in ["saldo", "zakup", "sprzedaz", "stop"]):
-            licznik += 1
-            if (licznik > int(sys.argv[3]) + 1):
-                break
-        print(linijka)
-        outPlik.write(linijka + "\n")
-
-    inPlik.close()
-    outPlik.close()
-
-else:
-    print("Upewnij sie, ze wprowadzone argumenty sa poprawne!")
-
-print("python", end=" ")
-for arg in sys.argv:
-    print(arg, end=" ")
+if sys.argv[1] == "balance":
+    print(account["balance"])
